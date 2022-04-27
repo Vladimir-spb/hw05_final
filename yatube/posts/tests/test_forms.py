462587025f1a32d412e37966afed9740ceb2047c
Владимир Коршак <reos007@mail.ru>
2022-04-27 18:46:56 +0300

    Rev4

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,5 +1,9 @@
+import tempfile
+
 from django.contrib.auth import get_user_model
-from django.test import TestCase, Client
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.conf import settings
+from django.test import TestCase, Client, override_settings
 from django.urls import reverse
 
 from ..models import Group, Post, Comment
@@ -7,6 +11,10 @@ from ..models import Group, Post, Comment
 User = get_user_model()
 
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
+
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class CreateFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -104,3 +112,30 @@ class CreateFormTests(TestCase):
                          .count(), comment_count + 1)
         self.assertEqual(comment_create_0.text, form_data['text'])
         self.assertEqual(comment_create_0.author, self.user_2)
+
+    def test_upload_image(self):
+        """Тест на создание поста с картинкой"""
+        image = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        upload = SimpleUploadedFile(
+            name="image.png",
+            content=image,
+            content_type="small/png"
+        )
+        text_post = 'Пост с картинкой'
+        form_data = {
+            'text': text_post,
+            'image': upload,
+        }
+        self.authorized_client.post(
+            reverse('posts:post_create'), data=form_data, follow=True
+        )
+        post_create_image = Post.objects.order_by('-id')[0]
+        self.assertEqual(post_create_image.text, form_data['text'])
+        self.assertTrue(post_create_image.image)
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,19 +1,13 @@
-import tempfile
-
 from django.core.cache import cache
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase, override_settings
+from django.test import Client, TestCase
 from django.urls import reverse
 from django import forms
 
 from ..models import Group, Post, Follow
 
 User = get_user_model()
-
-
-TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
 class PaginatorViewsTest(TestCase):
@@ -66,7 +60,6 @@ class PaginatorViewsTest(TestCase):
                 self.assertEqual(len(response.context['page_obj']), template)
 
 
-@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostViewTest(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -172,37 +165,6 @@ class PostViewTest(TestCase):
         first_object = response.context['post']
         post_image_0 = first_object.image
         self.assertEqual(post_image_0, self.post.image)
-
-    def test_upload_image(self):
-        """Тест на создание поста с картинкой"""
-        image = (
-            b'\x47\x49\x46\x38\x39\x61\x02\x00'
-            b'\x01\x00\x80\x00\x00\x00\x00\x00'
-            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-            b'\x0A\x00\x3B'
-        )
-        upload = SimpleUploadedFile(
-            name="image.png",
-            content=image,
-            content_type="small/png"
-        )
-        text_post = 'Пост с картинкой'
-        form_data = {
-            'text': text_post,
-            'image': upload,
-        }
-        self.authorized_client.post(
-            reverse('posts:post_create'), data=form_data, follow=True
-        )
-        post_create_image = Post.objects.order_by('-id')[0]
-        self.assertTrue(post_create_image.image)
-        self.assertTrue(Post.objects.filter(
-            text='Пост с картинкой',
-            image='posts/image.png'
-        ).exists()
-        )
 
 
 class CashTest(TestCase):
